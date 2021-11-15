c449593faf4d1e6e97232a7402abdf9a4e74995a
shvetsov daniil <shvetsov.daniil@stmd.pro>
2021-11-15 15:34:13 +0500

    creates basic html for auth pages

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title') Вход @endsection
+@section('content')
+
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title') Регистрация @endsection
+@section('content')
+
+@endsection
