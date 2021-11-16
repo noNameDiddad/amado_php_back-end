269d0c2ca8e254c315cc3c0f822bacb5b2e0944a
shvetsov daniil <shvetsov.daniil@stmd.pro>
2021-11-16 15:44:01 +0500

    creates product seeder

--- database/migrations/2021_11_16_065121_create_products_table.php
+++ database/migrations/2021_11_16_065121_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product');
+            $table->string('number');
+            $table->string('price');
             $table->timestamps();
         });
     }
