b983026aac781fcb7e208627c8853c4f38397b83
shvetsov daniil <shvetsov.daniil@stmd.pro>
2021-11-19 13:57:54 +0500

    set role for users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => bcrypt('654321'), // password
             'remember_token' => Str::random(10),
+            'role' => 0,
         ];
     }
 
