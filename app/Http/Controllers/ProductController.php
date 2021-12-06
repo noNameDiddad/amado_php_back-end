2db900c8162e7f83dda9202d7c1728258868d691
shvetsov daniil <shvetsov.daniil@stmd.pro>
2021-12-06 16:02:10 +0500

    refactoring product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\ProductRequest;
 use App\Models\Product;
 use App\Models\UserProduct;
 use App\Notifications\PictureAdded;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
@@ -17,7 +18,7 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function index(Request $request, Cache $cache)
     {
@@ -46,19 +47,6 @@ class ProductController extends Controller
         ));
     }
 
-    public function filter($request, $isDesc, $sort_by)
-    {
-        $max_price = $request->max_price;
-        $category = $request->category;
-        $products = Product::with('category')
-            ->where('price', '<', $max_price)
-            ->orderBy($sort_by, $isDesc);
-        if ($category !== null) {
-            $products = $products->where('category_id', $category);
-        }
-        return $products->paginate(9);
-    }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -79,21 +67,10 @@ class ProductController extends Controller
      */
     public function store(ProductRequest $request, Cache $cache)
     {
-        $cache->cacheForget();
-
-        if ($file = $request->file('image')) {
-            $upload_folder = 'public/images';
-            $path = $file->store($upload_folder);
-            $name = str_replace($upload_folder . "/", '', $path);
-            $request->merge(['image_path' => $name]);
-        }
-
+        $cache->cacheForgetAll();
+        $request = $this->setFile($request);
         $product = Product::create($request->all());
-
-        if (App::environment(['development', 'local'])) {
-            Log::info("Product was created id=" . $product->id);
-            Log::info($product);
-        }
+        $this->logging("created", $product);
 
         return redirect()->route('product.index');
     }
@@ -107,6 +84,68 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         return view('production.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product, Cache $cache)
+    {
+        $this->authorize('update', $product);
+        $categories = $cache->cacheCategoryAll();
+        return view('production.edit', compact(['categories', 'product']));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Product $product
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(ProductRequest $request, Product $product,  Cache $cache)
+    {
+        $this->authorize('update', $product);
+        $cache->cacheForgetAll();
+        $request = $this->setFile($request);
+        $this->deleteFile($product->image_path);
+        $product = Product::update($request->all());
+        $this->logging("updated", $product);
+
+        return redirect()->route('product.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product, Cache $cache)
+    {
+        $this->authorize('delete', $product);
+        $cache->cacheForgetAll();
+        $this->logging("deleted", $product);
+        $this->deleteFile($product->image_path);
+        $product->delete();
+
+        return redirect()->back();
+    }
+
+    public function filter($request, $isDesc, $sort_by)
+    {
+        $max_price = $request->max_price;
+        $category = $request->category;
+        $products = Product::with('category')
+            ->where('price', '<', $max_price)
+            ->orderBy($sort_by, $isDesc);
+        if ($category !== null) {
+            $products = $products->where('category_id', $category);
+        }
+        return $products->paginate(9);
     }
 
     public function takeProduct(Request $request, $product_id)
@@ -125,68 +164,28 @@ class ProductController extends Controller
         return redirect()->back();
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Models\Product $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product, Cache $cache)
+    public function setFile(ProductRequest $request)
     {
-        $this->authorize('update', $product);
-        $categories = $cache->cacheCategoryAll();
-        return view('production.edit', ['categories' => $categories, 'product' => $product]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Product $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(ProductRequest $request, Product $product,  Cache $cache)
-    {
-        $this->authorize('update', $product);
-
-        $cache->cacheForgetAll();
-
         if ($file = $request->file('image')) {
             $upload_folder = 'public/images';
-            $filename = $file->getClientOriginalName();
-
-            Storage::putFileAs($upload_folder, $file, $filename);
-
-            $product->image_path = $filename;
+            $path = $file->store($upload_folder);
+            $name = str_replace($upload_folder . "/", '', $path);
+            $request->merge(['image_path' => $name]);
         }
-        if (App::environment(['development', 'local'])) {
-            Log::info("Product was updated id=" . $product->id);
-            Log::info($product);
-        }
-        $product->update();
-
-        return redirect()->route('product.index');
+        return $request;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Product $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product, Cache $cache)
+    public function deleteFile($file)
     {
-        $this->authorize('delete', $product);
-        $cache->cacheForgetAll();
+        Storage::delete("public/images/" . $file);
+    }
+
+    public function logging($action, $product)
+    {
         if (App::environment(['development', 'local'])) {
-            Log::info("Product was deleted id=" . $product->id);
+            Log::info("Product was ".$action." id=" . $product->id);
             Log::info($product);
         }
-        $file = $product->image_path;
-        $product->delete();
-        Storage::delete("public/images/" . $file);
-
-        return redirect()->back();
     }
 
 
